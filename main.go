64276c03da997d20ffbd38228825af12156ab99c
Anton Biluta <AntonBiluta@gmail.com>
2023-09-16 23:48:30 +0300

    #4: Добавление интерфейса командной строки

--- main.go
+++ main.go
@@ -1,23 +1,95 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/antonbiluta/blockchain-golang/blockchain"
+	"github.com/dgraph-io/badger"
+	"os"
+	"runtime"
+	"strconv"
 )
+
+type CommandLine struct {
+	blockchain *blockchain.Blockchain
+}
+
+func (cli *CommandLine) printUsage() {
+	fmt.Println("Использование:")
+	fmt.Println("  add -block BLOCK_DATA - добавить блок в цепочку")
+	fmt.Println("  print - Напечатать блоки из цепи")
+}
+
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		runtime.Goexit()
+	}
+}
+
+func (cli *CommandLine) addBlock(data string) {
+	cli.blockchain.AddBlock(data)
+	fmt.Println("Added block!")
+}
+
+func (cli *CommandLine) printChain() {
+	iter := cli.blockchain.Iterator()
+	for {
+		block := iter.Next()
+		fmt.Printf("PrevHash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevHash) == 0 {
+			break
+		}
+	}
+}
+
+func (cli *CommandLine) run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+	addBlockData := addBlockCmd.String("block", "", "Block data")
+
+	switch os.Args[1] {
+	case "add":
+		err := addBlockCmd.Parse(os.Args[2:])
+		blockchain.HandleError(err)
+
+	case "print":
+		err := printChainCmd.Parse(os.Args[2:])
+		blockchain.HandleError(err)
+
+	default:
+		cli.printUsage()
+		runtime.Goexit()
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
 
 func main() {
 	chain := blockchain.InitBlockchain()
+	defer func(Database *badger.DB) {
+		err := Database.Close()
+		blockchain.HandleError(err)
+	}(chain.Database)
 
-	chain.AddBlock("Test1")
-	chain.AddBlock("Test2")
-	chain.AddBlock("Test3")
-
-	//for _, block := range chain {
-	//	fmt.Printf("Prev Hash: %x\n", block.PrevHash)
-	//	fmt.Printf("Data: %s\n", block.Data)
-	//	fmt.Printf("Hash: %x\n", block.Hash)
-	//
-	//	pow := blockchain.NewProof(block)
-	//	fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-	//	fmt.Println()
-	//}
+	cli := CommandLine{chain}
+	cli.run()
 }
